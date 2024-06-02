ffb735fadbf70c5987ed494aca16b484c57f50f3
Sova <108209320+NceSova@users.noreply.github.com>
2024-06-02 20:23:35 +0300

    Added playRound() logic

--- script.js
+++ script.js
@@ -4,11 +4,48 @@ function getComputerChoice() {
   if(rand<6.66){return 'paper'};
   if(rand<10){return 'scissors'};
 }
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-  let choice = prompt();
+  let choice = prompt().toLowerCase();
   return choice;
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  let result = 'oops';
+  if (humanChoice === computerChoice) {
+    result = 'Its a tie!'
+  }
+
+  if (humanChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      result = `You lose! Paper beats Rock!`
+    } else
+    if (computerChoice === 'scissors') {
+      result = 'You won! Rock beats Scissors!'
+    }
+  } else 
+  if (humanChoice === 'paper') {
+    if (computerChoice === 'rock') {
+      result = 'You won! Paper beats Rock!'
+    } else
+    if (computerChoice === 'scissors') {
+      result = `You lose! Scissors beats Paper!`
+    }
+  } else
+  if (humanChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      result = `You lose! Rock beats Scissors!`
+    } else
+    if (computerChoice === 'paper') {
+      result = `You win! Scissors beats Paper!`
+    }
+  }
+
+  return result;
+}
+
+console.log(playRound(getHumanChoice(), getComputerChoice()))
